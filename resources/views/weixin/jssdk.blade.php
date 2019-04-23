eecb97f3affae0b5ab6baf8ac884339eb63d257f
liyijiehaha <2632379213@qq.com>
2019-04-23 11:59:01 +0000

    1

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -11,20 +11,15 @@
 </head>
 <body>
 <button id="btn1">选择照片</button>
-<img src="" alt="" id="imgs0" width="300">
-<hr>
-<img src="" alt="" id="imgs1"  width="300">
-<hr>
-<img src="" alt="" id="imgs2"  width="300">
 </body>
 </html>
 <script>
     wx.config({
-        //debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: "{{$wx_config['appId']}}", // 必填，公众号的唯一标识
-        timestamp: "{{$wx_config['time']}}", // 必填，生成签名的时间戳
-        nonceStr: "{{$wx_config['nonce']}}", // 必填，生成签名的随机串
-        signature: "{{$jsconfig['signature']}}",// 必填，签名
+        timestamp: "{{$wx_config['timestamp']}}", // 必填，生成签名的时间戳
+        nonceStr: "{{$wx_config['nonceStr']}}", // 必填，生成签名的随机串
+        signature: "{{$wx_config['signature']}}",// 必填，签名
         jsApiList: ['chooseImage','uploadImage'] // 必填，需要使用的JS接口列表
     });
     wx.ready(function(){
@@ -36,10 +31,11 @@
                 success: function (res) {
                     var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                     var img='';
-                    $.echo(localIds,function(){
+                    $.echo(localIds,function(i,v){
                         img += v+',';
                         var node = "#imgs"+i;
                         $(node).attr('src',v);
+
                         //上传图片
                         wx.uploadImage({
                             localId: v, // 需要上传的图片的本地ID，由chooseImage接口获得
@@ -55,5 +51,6 @@
             });
         });
     });
+
 
 </script>
