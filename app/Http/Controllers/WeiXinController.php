8f8bd56c7166b2c7688cef97621a3f1cd1f5dce7
liyijiehaha <2632379213@qq.com>
2019-04-24 12:10:44 +0000

    1

--- app/Http/Controllers/WeiXinController.php
+++ app/Http/Controllers/WeiXinController.php
@@ -112,6 +112,7 @@ class WeiXinController extends Controller
     public function getu(){
         echo'<pre>';print_r($_GET);echo '</pre>';
         $code = $_GET['code'];
+        var_dump($code);
         //获取access_token
         $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
         $response = json_decode(file_get_contents($url),true);
