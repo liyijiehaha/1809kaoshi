1a1a9b259f29864d07c1da5117497071087c9fe3
liyijiehaha <2632379213@qq.com>
2019-04-24 11:59:28 +0000

    1

--- app/Http/Controllers/WeiXinController.php
+++ app/Http/Controllers/WeiXinController.php
@@ -116,5 +116,11 @@ class WeiXinController extends Controller
         $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
         $response = json_decode(file_get_contents($url),true);
         echo'<pre>';print_r($response);echo '</pre>';
+        $access_token=$response['access_token'];
+        $openid=$response['openid'];
+        //获取用户信息
+        $url='https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $res = json_decode(file_get_contents($url),true);
+        echo'<pre>';print_r($res);echo '</pre>';
     }
 }
